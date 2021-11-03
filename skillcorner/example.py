592d06d225e391d7c3050af76b403324602b439c
jpatacz <julia.patacz@skillcorner.com>
2021-11-03 17:01:36 +0100

    Adjusting example script

--- skillcorner/example.py
+++ skillcorner/example.py
@@ -1,18 +1,18 @@
 from skillcorner.client import SkillcornerClient
 
 # Create client object
-client = SkillcornerClient(username='', password='')
+client = SkillcornerClient(username='PUT_YOUR_LOGIN_HERE', password='PUT_YOUR_PASSWORD_HERE')
 
 # Get data from simple endpoint
-data = client.get_matches()
+data = client.get_competitions()
 print(data['count'])
+
+# Get and save data from simple endpoint
+client.get_and_save_competitions(filepath="competitions.json", params={'season': 5})
 
 # Get data from endpoint using object id
 data = client.get_match(match_id=49364)
 print(data)
-
-# Get and save data from simple endpoint
-client.get_and_save_matches(filepath='matches.json')
 
 # Get and save data from endpoint using object id
 client.get_and_save_match(match_id=49364, filepath='match.json')
@@ -23,8 +23,12 @@ data = client.get_physical(params={'season': 8, 'team': 1415})
 print(data)
 client.get_and_save_physical(filepath="physical.json", params={'season': 6, 'team': 1415})
 
+data = client.get_matches(params={'season': 6, 'team': 327})
+print(data)
+client.get_and_save_matches(params={'competition_edition': 171, 'date_time__gt': '2021-02-10'})
+
 # Use all remaining methods
-data = client.get_teams()
+data = client.get_teams(params={'competition_edition': 99})
 print(data['count'])
 client.get_and_save_teams(filepath="teams.json")
 
@@ -32,17 +36,13 @@ data = client.get_team(team_id=2)
 print(data)
 client.get_and_save_team(team_id=2, filepath="team.json")
 
-data = client.get_players()
+data = client.get_players(params={'team': 481, 'competition_edition': 115})
 print(data['count'])
-client.get_and_save_players(filepath="players.json")
+client.get_and_save_players(filepath="players.json", params={'team': 481, 'competition_edition': 115})
 
 client.get_player(player_id=38759)
 print(data)
 client.get_and_save_player(player_id=38759, filepath="player.json")
-
-data = client.get_competitions()
-print(data['count'])
-client.get_and_save_competitions(filepath="competitions.json")
 
 data = client.get_competition_editions(competition_id=4)
 print(data['count'])
