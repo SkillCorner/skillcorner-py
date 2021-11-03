0191ba9e13a8e6c4434c97bb403629155dec0cf6
jpatacz <julia.patacz@skillcorner.com>
2021-11-03 16:20:05 +0100

    Improvements for github actions workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='skillcorner',
-    version='0.1.0',
+    version='1.0.0',
     description='SkillCorner API client',
     author='SkillCorner',
     author_email='support@skillcorner.com',
