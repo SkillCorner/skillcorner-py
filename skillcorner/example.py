ff2cbc8a32db9767b78b26d98fd1fe39effa28cf
jpatacz <julia.patacz@skillcorner.com>
2021-11-04 17:58:34 +0100

    Final improvements in example script

--- skillcorner/example.py
+++ skillcorner/example.py
@@ -5,7 +5,7 @@ client = SkillcornerClient(username='PUT_YOUR_LOGIN_HERE', password='PUT_YOUR_PA
 
 # Get data from simple endpoint
 data = client.get_competitions()
-print(data['count'])
+print(data)
 
 # Get and save data from simple endpoint
 client.get_and_save_competitions(filepath="competitions.json", params={'season': 5})
@@ -19,17 +19,17 @@ client.get_and_save_match(match_id=49364, filepath='match.json')
 
 # Usage of extra 'params' parameter
 # Get physical data using 'params' parameter
-data = client.get_physical(params={'season': 8, 'team': 1415})
+data = client.get_physical(params={'season': 7, 'team': 4})
 print(data)
-client.get_and_save_physical(filepath="physical.json", params={'season': 6, 'team': 1415})
+client.get_and_save_physical(filepath="physical.json", params={'season': 8, 'team': 76})
 
 data = client.get_matches(params={'season': 6, 'team': 327})
 print(data)
-client.get_and_save_matches(params={'competition_edition': 171, 'date_time__gt': '2021-02-10'})
+client.get_and_save_matches(filepath="matches.json", params={'competition_edition': 171, 'date_time__gt': '2021-02-10'})
 
 # Use all remaining methods
-data = client.get_teams(params={'competition_edition': 99})
-print(data['count'])
+data = client.get_teams(params={'competition_edition': 141})
+print(data)
 client.get_and_save_teams(filepath="teams.json")
 
 data = client.get_team(team_id=2)
@@ -37,7 +37,7 @@ print(data)
 client.get_and_save_team(team_id=2, filepath="team.json")
 
 data = client.get_players(params={'team': 481, 'competition_edition': 115})
-print(data['count'])
+print(data)
 client.get_and_save_players(filepath="players.json", params={'team': 481, 'competition_edition': 115})
 
 client.get_player(player_id=38759)
@@ -45,16 +45,17 @@ print(data)
 client.get_and_save_player(player_id=38759, filepath="player.json")
 
 data = client.get_competition_editions(competition_id=4)
-print(data['count'])
+print(data)
 client.get_and_save_competition_editions(competition_id=4, filepath="competition_editions.json")
 
 data = client.get_match_tracking_data(match_id=49364)
 print(data)
-data = client.get_match_video_tracking_data(match_id=49364)
+data = client.get_match_video_tracking_data(match_id=60113, params={"frame__gt": 60000})
 print(data)
-data = client.get_match_data_collection(match_id=49364)
+data = client.get_match_data_collection(match_id=62100)
 print(data)
 
 client.get_and_save_match_tracking_data(match_id=49364, filepath="tracking_data.json")
-client.get_and_save_match_video_tracking_data(match_id=49364, filepath="video_tracking_data.json")
-client.get_and_save_match_data_collection(match_id=49364, filepath="data_collection.json")
+client.get_and_save_match_video_tracking_data(match_id=60113, filepath="video_tracking_data.json",
+                                              params={"frame__gt": 60000})
+client.get_and_save_match_data_collection(match_id=62100, filepath="data_collection.json")
